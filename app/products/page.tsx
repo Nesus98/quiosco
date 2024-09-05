2b3f8f26adc18fd325a17086cc5dee6f89b36f97
Nesus98 <jesusarroyo30.98@gmail.com>
2024-09-05 13:18:13 +0200

    admin page and rrefetching when order completes

--- app/products/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function ProductsPage() {
-
-  
-
-  return (
-    <h1>Hello world page!</h1>
-  )
-}
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,11 +3,20 @@ import { z } from "zod";
 export const OrderSchema = z.object({
   name: z.string().min(1, "El Nombre es Obligatorio"),
   total: z.number().min(1, "Hay errores en la orden"),
-  order: z.array(z.object({
-    id: z.number(),
-    name: z.string(),
-    price: z.number(),
-    quantity: z.number(),
-    subtotal: z.number()
-  }))
+  order: z.array(
+    z.object({
+      id: z.number(),
+      name: z.string(),
+      price: z.number(),
+      quantity: z.number(),
+      subtotal: z.number(),
+    })
+  ),
+});
+
+export const OrderIdSchema = z.object({
+  orderId: z
+    .string()
+    .transform((value) => parseInt(value))
+    .refine((value) => value > 0, { message: "Hay errores" }),
 });
